dab92f3b0d6d507a64f5cdb2a14fa06ce45c938a
Mona Alahmadi <miraidreem@gmail.com>
2020-12-16 19:38:27 +0300

    m

--- app.py
+++ app.py
@@ -214,11 +214,11 @@ def create_app(test_config=None):
 
   return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
   #port = int(os.environ.get("PORT", 5000))
   #print(port)
-  APP.run()
+  app.run()
 
 
